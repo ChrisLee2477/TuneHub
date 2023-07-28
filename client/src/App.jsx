4df4cf40880f54bc0c859e48e0052755f0148c69
Arsalan <arsalank.ansari@gmail.com>
2023-07-28 15:26:08 -0400

    Nav bar done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,13 @@ import Spotify from "../src/components/Spotify";
 import Chat from "./components/Chat";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Nav from "./components/Nav";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Nav />
       <Chat />
       <Login />
       <Signup />
