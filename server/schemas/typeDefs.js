eae17a5d336cc3e2ae0c479c373f7b5c968a9753
tegaomare <tegaomare@yahoo.com>
2023-07-28 12:24:18 -0400

    user,song,playlist and comment added

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,53 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    
+  }
+ 
+  type Song = {
+    _id: string!
+    title: string!
+    artist: string!
+    album: string!
+    duration: int!
+  }
+
+  type Playlist = {
+    _id: string!
+    name: string!
+    creator: User!
+    songs: [Song!]!
+  }
+
+  type Comment {
+    _id: ID!
+    user: User!
+    content: String!
+    timestamp: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+  getUserById(id: ID!): User
+  getAllSongs: [Song!]!
+  getPlaylistById(id: ID!): Playlist
+ 
+}
+
+type Mutation {
+  createUser(username: String!, email: String!, password: String!): User
+  createSong(title: String!, artist: String!, album: String!, duration: Int!): Song
+  createPlaylist(name: String!, creatorId: ID!): Playlist
+  addSongToPlaylist(playlistId: ID!, songId: ID!): Playlist
+  postComment(playlistId: ID!, userId: ID!, content: String!): Comment
+ 
+}
+  `;
+
+module.exports = typeDefs;
