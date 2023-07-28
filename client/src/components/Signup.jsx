6d1f7aa86b27d54d3f52dd90cd0d7416efd3826d
Arsalan <arsalank.ansari@gmail.com>
2023-07-28 15:09:44 -0400

    name added in form

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -4,6 +4,11 @@ import Form from "react-bootstrap/Form";
 export default function Login() {
   return (
     <Form>
+      <Form.Group className="mb-3" controlId="formName">
+        <Form.Label>Name:</Form.Label>
+        <Form.Control type="User" placeholder="Full Name" />
+      </Form.Group>
+
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
