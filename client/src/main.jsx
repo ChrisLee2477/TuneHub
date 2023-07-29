c7b2ca2df5c137d5ac57576336be16947b83469f
tegaomare <tegaomare@yahoo.com>
2023-07-29 12:43:26 -0400

    seed bugs fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,38 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-// import './index.css'
+import Songs from './pages/Songs.jsx';
+import Playlist from './pages/Playlist.jsx';
+import Signup from './pages/Signup.jsx';
+import Login from './pages/Login.jsx';
+import SpotifyPage from './pages/SpotifyPage.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Songs />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        element: <Playlist />
+      }, {
+        element: <SpotifyPage />
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
