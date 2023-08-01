3881fbe036bdaa09d1a3354d05e97260297996c6
Arsalan <arsalank.ansari@gmail.com>
2023-08-01 12:20:23 -0400

    adjusted the placement of forms

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,11 +3,12 @@ import React from "react";
 // import Signup from "../components/Signup";
 import Comment from "./Comment";
 import Spotify from "../components/Spotify";
+import "../components/Components.css";
 
 export default function Dashboard() {
   return (
     <>
-      <header>Welcome To TuneHubs!!</header>
+      <header className="header">Welcome To TuneHubs!!</header>
       <Comment />
       <Spotify />
     </>
